07ee15f4e54a9b4f271e972e9e20830bc702516c
CarlosVG19 <201809396@est.umss.edu>
2021-04-23 05:50:35 -0400

    Clase LaberintoC
    -Metodo generarCamino agregado

---
+++ src/main/java/laberintoc/Punto.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laberintoc;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Punto {
+    private int valor;
+    private boolean visitada;
+    private int posX;
+    private int posY;
+
+    public Punto(int valor, boolean visitada, int posX, int posY) {
+        this.valor = valor;
+        this.visitada = visitada;
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+    public Punto() {
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public boolean getVisitada() {
+        return visitada;
+    }
+
+    public void setVisitada(boolean visitada) {
+        this.visitada = visitada;
+    }
+    
+    public Punto getSiguiente(Punto punto)
+    {
+        Punto aux = new Punto();
+        if (punto.getPosY() + 1 == 0)
+        {
+            
+        }
+        return aux;
+    }
+}
