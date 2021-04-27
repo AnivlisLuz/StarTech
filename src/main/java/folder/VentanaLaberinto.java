628736fb37c6bfdc4ee9f4cc40ccf08c47b68cba
Johel-code <201910122@est.umss.edu>
2021-04-26 23:34:45 -0400

    restaurar vidas

--- src/main/java/folder/VentanaLaberinto.java
+++ src/main/java/folder/VentanaLaberinto.java
@@ -249,6 +249,7 @@ public class VentanaLaberinto extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        laberinto.restaurarVidas();
         dispose();
         ventanaLevel.setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
