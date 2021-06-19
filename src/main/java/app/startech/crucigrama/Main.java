ce64a490b3bafb0fa2bbcc781dfb3ea4182927b1
CarlosVG19 <201809396@est.umss.edu>
2021-06-19 19:22:08 -0400

    Quitar números
    Ahora el método para quitar números es:
    Primero se seleccionan todas las casillas de operaciones.
    Luego se quita un solo número por cada operación para que estas siempre puedan resolverse.

--- src/main/java/app/startech/crucigrama/Main.java
+++ src/main/java/app/startech/crucigrama/Main.java
@@ -12,7 +12,7 @@ package app.startech.crucigrama;
 public class Main {
     public static void main(String[] args)
     {
-        MatrizCrucigrama mat = new MatrizCrucigrama("division");
+        MatrizCrucigrama mat = new MatrizCrucigrama("suma");
         char[][] matriz = mat.getMat();
         char[][] matrizRes = mat.getMatRes();
         
