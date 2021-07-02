cfc99e831b68a15a39e65061b83e053237a97e6d
Johel-code <201910122@est.umss.edu>
2021-07-02 18:01:14 -0400

    laberinto desing

--- src/main/java/app/startech/laberintos/Laberinto.java
+++ src/main/java/app/startech/laberintos/Laberinto.java
@@ -5,6 +5,9 @@ public class Laberinto {
     private final String titulo;
     private final String condicion;
     private final int nivel;
+    private String dir1;
+    private String dir2;
+    private String dirFondo;
     private int vidas;
     private final String mensaje;
     private Matriz matriz;
@@ -15,6 +18,18 @@ public class Laberinto {
         this.titulo = titulo;
         vidas = 3;
         this.mensaje = mensaje;
+    }
+
+    public String getDir1() {
+        return dir1;
+    }
+
+    public String getDir2() {
+        return dir2;
+    }
+
+    public String getDirFondo() {
+        return dirFondo;
     }
 
     //Retorna la condición. Ejem: Suma de uno en uno
@@ -50,7 +65,10 @@ public class Laberinto {
     //Genera el laberinto según la categoría (título) y el nivel
     public void mostrarMatriz() {
         if (titulo.equals("Secuencia")) {       // laberinto Secuencia tendria las siguientes matrices:
-            if (nivel == 1) {                   // lvl 1 = tamaño 3                 
+            dir2 = "C:\\Users\\Criss\\Documents\\GitHub\\StarTech\\src\\main\\java\\app\\startech\\imagenes\\decorate\\7.png";
+            dir1 = "C:\\Users\\Criss\\Documents\\GitHub\\StarTech\\src\\main\\java\\app\\startech\\imagenes\\decorate\\8.png";
+            dirFondo = "C:\\Users\\Criss\\Documents\\GitHub\\StarTech\\src\\main\\java\\app\\startech\\imagenes\\fondo1.4.jpg";
+            if (nivel == 1) {                   // lvl 1 = tamaño 3 
                 matriz = new Secuencia(3);      // lvl 2 = tamaño 4
                 matriz.generarLaberinto();      // lvl 3 = tamaño 6
                 matriz.rellenarLaberinto();     // lvl 4 = tamaño 8
@@ -65,6 +83,9 @@ public class Laberinto {
                 }
             }
         } else if (titulo.equals("Suma")) {         // laberinto Suma todas sus matrices son de tamaño 6
+            dir2 = "C:\\Users\\Criss\\Documents\\GitHub\\StarTech\\src\\main\\java\\app\\startech\\imagenes\\decorate\\3.png";
+            dir1 = "C:\\Users\\Criss\\Documents\\GitHub\\StarTech\\src\\main\\java\\app\\startech\\imagenes\\decorate\\4.png";
+            dirFondo = "C:\\Users\\Criss\\Documents\\GitHub\\StarTech\\src\\main\\java\\app\\startech\\imagenes\\fondo1.2.jpg";
             for (int i = 1; i < 10; i++) {          // su condición sube de acuerdo al nivel de 2 a 10
                 if (nivel == i) {
                     matriz = new Suma(6, i + 1);
@@ -74,6 +95,9 @@ public class Laberinto {
                 }
             }
         } else if (titulo.equals("Resta")) {        // laberinto Resta todas sus matrices son de tamaño 6
+            dir2 = "C:\\Users\\Criss\\Documents\\GitHub\\StarTech\\src\\main\\java\\app\\startech\\imagenes\\decorate\\5.png";
+            dir1 = "C:\\Users\\Criss\\Documents\\GitHub\\StarTech\\src\\main\\java\\app\\startech\\imagenes\\decorate\\6.png";
+            dirFondo = "C:\\Users\\Criss\\Documents\\GitHub\\StarTech\\src\\main\\java\\app\\startech\\imagenes\\fondo1.3.jpg";
             for (int i = 1; i < 11; i++) {          // su condición sube de acuerdo al nivel de 1 a 10
                 if (nivel == i) {
                     matriz = new Resta(6, i);
@@ -83,6 +107,9 @@ public class Laberinto {
                 }
             }
         } else if (titulo.equals("Multiplicación")) {   // laberinto Multiplicación todas sus matrices son de tamaño 10
+            dir2 = "C:\\Users\\Criss\\Documents\\GitHub\\StarTech\\src\\main\\java\\app\\startech\\imagenes\\decorate\\1.png";
+            dir1 = "C:\\Users\\Criss\\Documents\\GitHub\\StarTech\\src\\main\\java\\app\\startech\\imagenes\\decorate\\2.png";
+            dirFondo = "C:\\Users\\Criss\\Documents\\GitHub\\StarTech\\src\\main\\java\\app\\startech\\imagenes\\fondo1.1.jpg";
             for (int i = 1; i < 10; i++) {              // su condición sube de acuerdo al nivel de 2 a 10
                 if (nivel == i) {
                     matriz = new Multiplicacion(10, i + 1);
@@ -92,6 +119,9 @@ public class Laberinto {
                 }
             }
         } else if (titulo.equals("División")) {         // laberinto Divición todas sus matrices son de tamaño 10
+            dir2 = "C:\\Users\\Criss\\Documents\\GitHub\\StarTech\\src\\main\\java\\app\\startech\\imagenes\\decorate\\9.png";
+            dir1 = "C:\\Users\\Criss\\Documents\\GitHub\\StarTech\\src\\main\\java\\app\\startech\\imagenes\\decorate\\10.png";
+            dirFondo = "C:\\Users\\Criss\\Documents\\GitHub\\StarTech\\src\\main\\java\\app\\startech\\imagenes\\fondo1.5.jpg";
             for (int i = 1; i < 10; i++) {              // su condición sube de acuerdo al nivel de 2 a 10
                 if (nivel == i) {
                     matriz = new Divicion(10, i + 1);
